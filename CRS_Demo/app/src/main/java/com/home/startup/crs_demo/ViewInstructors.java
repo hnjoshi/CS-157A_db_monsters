44afa48d2a6c2efe17c53fd97c0b316846909675
AKO <ko.alvin.1748@gmail.com>
2014-12-03 22:51:50 -0800

    Cannot get ViewInstructors to work
    Added activity to Android Manifest xml file.
    Appended the new activity to strings.xml.
    Edited the activity file for viewing instructors.

--- CRS_Demo/app/src/main/java/com/home/startup/crs_demo/ViewInstructors.java
+++ CRS_Demo/app/src/main/java/com/home/startup/crs_demo/ViewInstructors.java
@@ -1,11 +1,16 @@
 package com.home.startup.crs_demo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.sql.Connection;
@@ -17,36 +22,57 @@ import java.sql.ResultSet;
 public class ViewInstructors extends Activity {
 
     private ListView list;
-
+    private String sID;
     private String title[];
+    private String title2[];
     private String department[];
     private String term[];
-    //private String date[];
-    //private String time[];
+    private String cID[];
 
     private Bitmap thumbImg;
 
-    private String userID;
+
+
+    private String userChoice[];
+    private String userChoice2[];
+    private int userChoiceptr=0;
+    private String txt="";
+    private Connection conn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_view_enrolled);
+        setContentView(R.layout.activity_view_instructors);
 
         Login lg = new Login();
-        userID = lg.getUSER_ID();
+        sID = lg.getUSER_ID();
 
-        getEnrolledCourses();
+        //getEnrolledCourses();
 
         CustomList adapter = new CustomList(ViewInstructors.this, title, term, department, thumbImg);
         list=(ListView)findViewById(R.id.list);
+        /*
         list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                if(userChoiceptr<userChoice.length)
+                {
+                    userChoice[userChoiceptr] = cID[position];
+                    userChoice2[userChoiceptr] = title2[position];
+                    userChoiceptr++;
+
+                }
+            }
+
+        });*/
     }
 
     private void getEnrolledCourses()
     {
-        Connection conn = null;
         try {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
@@ -56,38 +82,39 @@ public class ViewInstructors extends Activity {
 
             PreparedStatement pst = null;
             ResultSet rs = null;
-            //Note: Change the script here to show instructors. Use cross join as specified in project design
-            pst = conn.prepareStatement("select a.Title, a.Term, a.StartTime, a.EndTime, a.StartDate, a.EndDate, d.Name\n" +
+            pst = conn.prepareStatement("select a.cID, a.Title, a.Term, a.StartTime, a.EndTime, a.StartDate, a.EndDate, d.Name\n" +
                     "from (select * from\n" +
                     "\t\t(select cID from enrollment where sID=?) courseID \n" +
                     "     natural join\n" +
                     "     course RegCourse) a\n" +
                     "join department d\n" +
                     "where a.dID=d.dID");
-            pst.setString(1, userID);
+            pst.setString(1, sID);
             rs = pst.executeQuery();
 
             rs.last();
             int count = rs.getRow();
             rs.beforeFirst();
-            //Log.w("Count",count+"");
+
+            userChoice = new String[count];
+            userChoice2 = new String[count];
 
             title = new String[count];
+            title2 = new String[count];
             department = new String[count];
             term = new String[count];
-            //date = new String[count];
-            //time = new String[count];
+            cID = new String[count];
 
             int c=0;
             while (rs.next()) {
+                cID[c] = rs.getString(1);
+                title[c] = "Class: "+rs.getString(2)+" Time:"+
+                        rs.getString(4)+" to "+rs.getString(5);
+                term[c] = rs.getString(3)+" "+rs.getString(6)+" to "+
+                        rs.getString(7);
+                department[c] = "Department: "+rs.getString(8);
 
-                title[c] = "Class: "+rs.getString(1)+" Time:"+
-                        rs.getString(3)+" to "+rs.getString(4);
-                term[c] = rs.getString(2)+" "+rs.getString(5)+" to "+
-                        rs.getString(6);
-                //time[c] = rs.getString(3)+" to "+rs.getString(4);
-                //date[c] = rs.getString(5)+" to "+rs.getString(6);
-                department[c] = "Department: "+rs.getString(7);
+                title2[c]=rs.getString(2);
 
                 c++;
             }
@@ -101,23 +128,25 @@ public class ViewInstructors extends Activity {
         }
         finally
         {
-            if(conn != null)
-            {
-                try
-                {
-                    conn.close();
-                }
-                catch (Exception e)
-                {
-                    //e.printStackTrace();
-                }
-                finally {
-                    conn = null;
-                }
-
-            }
-
+            closeCon();
         }
     }
 
+    private void closeCon()
+    {
+        if(conn != null)
+        {
+            try
+            {
+                conn.close();
+            }
+            catch (Exception e)
+            {
+                //e.printStackTrace();
+            }
+            finally {
+                conn = null;
+            }
+        }
+    }
 }
